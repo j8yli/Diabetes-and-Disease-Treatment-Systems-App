5430ec655479916ad7a229c1ad686c5cf78fcb1e
Sai Varsha Ravisankar <saivarsharavisankar@lawn-143-215-96-146.lawn.gatech.edu>
2025-11-09 17:27:51 -0500

    added ingredient picker using USDA JSON file

---
+++ GlucoseApp/GlucoseApp/SignupView.swift
@@ -0,0 +1,21 @@
+//
+//  SignupView.swift
+//  GlucoseApp
+//
+//  Created by Sai Varsha Ravisankar on 11/3/25.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        Text("Signup Page (Placeholder)")
+            .font(.title)
+            .foregroundColor(.gray)
+            .padding()
+    }
+}
+
+#Preview {
+    SignupView()
+}
